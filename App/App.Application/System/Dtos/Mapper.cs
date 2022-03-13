d7cce44d140d81ec15ccaf12f788320fd9ce0525
Ken <hucandy@163.com>
2022-03-13 07:51:49 +0000

    app init

---
+++ App/App.Application/System/Dtos/Mapper.cs
@@ -0,0 +1,10 @@
+﻿using Mapster;
+
+namespace App.Application;
+
+public class Mapper : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+    }
+}
---
+++ App/App.Application/System/Services/SystemService.cs
@@ -0,0 +1,11 @@
+﻿using Furion.DependencyInjection;
+
+namespace App.Application;
+
+public class SystemService : ISystemService, ITransient
+{
+    public string GetDescription()
+    {
+        return "让 .NET 开发更简单，更通用，更流行。";
+    }
+}
